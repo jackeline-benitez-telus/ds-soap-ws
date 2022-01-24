29737f0dcdd755b211634a5838eafba9b6fd261f
Jackeline Benitez <jackeline.benitez@telus.com>
2022-01-24 03:00:39 -0600

    RGB and CMYK done

--- src/main/java/org/jugvale/ola/jaxws/CalculadoraWS.java
+++ src/main/java/org/jugvale/ola/jaxws/CalculadoraWS.java
@@ -30,4 +30,31 @@ public class CalculadoraWS {
 	public String sayHello(String name){
     	return "Say Hello to " + name;
 	}
+	
+	@WebMethod
+	public String HexTo(String hex){
+		String newHex=hex.substring(1,7);
+		
+		/*TO RGB*/
+		double r,g,b;		
+		r = Integer.valueOf(newHex.substring(0,2),16);
+		g = Integer.valueOf(newHex.substring(2,4),16);
+		b = Integer.valueOf(newHex.substring(4,6),16);
+		
+		/*TO CMYK*/
+		double k,c,m,y,max,r2,g2,b2;
+		r2=(r/255);
+		g2=(g/255);
+		b2=(b/255);
+		max=Math.max(Math.max(r2,g2),b2);
+		
+		k=1-max;
+		c= (1-r2-k)/(1-k);
+		m=(1-g2-k)/(1-k);
+		y=(1-b2-k)/(1-k);
+		
+    	return "\nHEX:" + hex 
+    			+"\nRGB: "+r+","+g+","+b
+    			+"\nCMYK: "+Math.round(c*100)+"%, "+Math.round(m*100)+"%, "+Math.round(y*100)+"%, "+Math.round(k*100)+"%";
+	}
 }
